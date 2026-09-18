88e53bf2361ac68b1e95a5ed2db2b9ab6ab256d7
Betalgezia <137531095+Betalgezia@users.noreply.github.com>
2026-09-18 18:49:33 +0300

    test: cover invalid VLESS fingerprint fallback

--- app/src/test/java/com/betalgezia/omnivpn/vpn/SubscriptionParserTest.kt
+++ app/src/test/java/com/betalgezia/omnivpn/vpn/SubscriptionParserTest.kt
@@ -109,6 +109,13 @@ class SubscriptionParserTest {
     }
 
     @Test
+    fun invalidVlessFingerprintFallsBackToRandom() {
+        val uri = "vless://00000000-0000-0000-0000-000000000001@edge.example.com:443?security=tls&fp=not-a-real-fingerprint"
+        val raw = JSONObject(SubscriptionParser.parse(uri).single().rawConfig!!)
+        val utls = raw.getJSONObject("tls").getJSONObject("utls")
+        assertEquals("random", utls.getString("fingerprint"))
+    }
+    @Test
     fun securityNoneOverridesOtherVlessTlsParameters() {
         val uri = "vless://00000000-0000-0000-0000-000000000001@edge.example.com:443?security=none&sni=cdn.example.com&fp=chrome&pbk=AQIDBAUGBwgJCgsMDQ4PEBESExQVFhcYGRobHB0eHyA"
         val raw = JSONObject(SubscriptionParser.parse(uri).single().rawConfig!!)
