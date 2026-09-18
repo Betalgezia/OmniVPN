3675c63cde051a2088f407067caa895e3bb157a4
Betalgezia <137531095+Betalgezia@users.noreply.github.com>
2026-09-18 19:03:43 +0300

    chore: remove unused Compose imports

--- app/src/main/java/com/betalgezia/omnivpn/MainActivity.kt
+++ app/src/main/java/com/betalgezia/omnivpn/MainActivity.kt
@@ -11,10 +11,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
