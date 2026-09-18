1e40988610e08a35049b8c2a02e94c2fbb1357bd
Betalgezia <137531095+Betalgezia@users.noreply.github.com>
2026-09-19 01:08:19 +0300

    fix: import ConfigParser in subscription repository

--- app/src/main/java/com/betalgezia/omnivpn/data/SubscriptionRepository.kt
+++ app/src/main/java/com/betalgezia/omnivpn/data/SubscriptionRepository.kt
@@ -9,6 +9,7 @@ import com.betalgezia.omnivpn.data.local.toEntity
 import com.betalgezia.omnivpn.data.model.Node
 import com.betalgezia.omnivpn.data.model.Subscription
 import com.betalgezia.omnivpn.vpn.ConfigParser
+import com.betalgezia.omnivpn.vpn.ConfigParser
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
