90bb9ab7341073e8f40b1d3c4a989b6469bc42ad
Betalgezia <137531095+Betalgezia@users.noreply.github.com>
2026-09-19 01:08:14 +0300

    fix: opt in to experimental Material3 TopAppBar API

--- app/src/main/java/com/betalgezia/omnivpn/MainActivity.kt
+++ app/src/main/java/com/betalgezia/omnivpn/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -47,6 +48,7 @@ class MainActivity : ComponentActivity() {
         setContent { OmniVpnScreen() }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @OptIn(ExperimentalMaterial3Api::class)
     @androidx.compose.runtime.Composable
     private fun OmniVpnScreen(viewModel: MainViewModel = hiltViewModel()) {
